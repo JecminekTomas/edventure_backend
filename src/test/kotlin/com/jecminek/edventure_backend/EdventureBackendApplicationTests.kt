e35a22e2d83cb04dec4e3e208b3bf878f3e6f377
jecminek_tomas <jecmto@gmail.com>
2021-02-10 22:52:21 +0100

    INIT

---
+++ src/test/kotlin/com/jecminek/edventure_backend/EdventureBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jecminek.edventure_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdventureBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
