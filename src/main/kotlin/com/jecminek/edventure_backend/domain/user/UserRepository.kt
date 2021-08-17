f94108e29e9b8dac4804f808c99d2958b5742591
jecminek_tomas <jecmto@gmail.com>
2021-08-17 17:02:10 +0200

    fix: Forgotten one.

--- src/main/kotlin/com/jecminek/edventure_backend/domain/user/UserRepository.kt
+++ src/main/kotlin/com/jecminek/edventure_backend/domain/user/UserRepository.kt
@@ -1,18 +1,10 @@
 package com.jecminek.edventure_backend.domain.user
 
-import com.jecminek.edventure_backend.enums.Faculty
-import com.jecminek.edventure_backend.enums.University
-import com.jecminek.edventure_backend.enums.UserRole
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findUserByRoles(role: UserRole, page: Pageable): Page<User>
+interface UserRepository : PagingAndSortingRepository<User, Long> {
 
     // TODO: 17.08.2021 GET RID OF QUERY
 
